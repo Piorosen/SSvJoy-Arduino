d7c3c5ff1f8b5f1fe383c2d3990c0cc0b0fbc2bf
Piorosen <chacha@udon.party>
2023-07-05 21:24:50 +0900

    Update SSvJoy.ino
    #5 했어용!

--- SSvJoy.ino
+++ SSvJoy.ino
@@ -9,17 +9,50 @@ void setup() {
   Serial.begin(19200);
 	Joystick.begin();
   callback = [](Type type, byte id, Data data, byte checksum, bool chk) {
-    // Serial.print("Type : ");
-    // Serial.print((int)type);
-    // Serial.print("\tData : ");
-    // Serial.print(data.Axis);
-    // Serial.print(" : ");
-    // Serial.print(data.Value);
-    // Serial.print("\tCheck : ");
-    // Serial.print(checksum);
-    // Serial.print(" : ");
-    // Serial.print(chk);
-    // Serial.print("\n");
+    switch (type) {
+      case Type::Button:
+        Joystick.setButton(id, data.Axis);
+        break;
+      case Type::AxisX:
+        Joystick.setXAxis(data.Axis);
+        break;
+      case Type::AxisY:
+        Joystick.setYAxis(data.Axis);
+        break;
+      case Type::AxisRotX:
+        Joystick.setRxAxis(data.Axis);
+        break;
+      case Type::AxisRotY:
+        Joystick.setRyAxis(data.Axis);
+        break;
+      case Type::Rudder:
+        Joystick.setRudder(data.Axis);
+        break;
+      case Type::Throttle:
+        Joystick.setThrottle(data.Axis);
+        break;
+      case Type::Accelerator:
+        Joystick.setAccelerator(data.Axis);
+        break;
+      case Type::Brake:
+        Joystick.setBrake(data.Axis);
+        break;
+      case Type::Steering:
+        Joystick.setSteering(data.Axis);
+        break;
+    }
+
+    Serial.print("Type : ");
+    Serial.print((int)type);
+    Serial.print("\tData : ");
+    Serial.print(data.Axis);
+    Serial.print(" : ");
+    Serial.print(data.Value);
+    Serial.print("\tCheck : ");
+    Serial.print(checksum);
+    Serial.print(" : ");
+    Serial.print(chk);
+    Serial.print("\n");
   };
   error = []() { 
     Serial.print("Error !!!\n");
